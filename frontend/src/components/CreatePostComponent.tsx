761eca1797dc9af74354063dd0f3dd2c74494365
agshruti12 <agshruti2901@gmail.com>
2024-05-11 16:00:22 -0400

    adding upload image function createpostcomponent

--- frontend/src/components/CreatePostComponent.tsx
+++ frontend/src/components/CreatePostComponent.tsx
@@ -1,46 +1,61 @@
-import React, { useState, useRef } from 'react';
-import axios from 'axios';
-import config from '../../config.json';
-import { useParams } from 'react-router-dom';
+import React, { useState, useRef } from "react";
+import axios from "axios";
+import config from "../../config.json";
+import { useParams } from "react-router-dom";
 import "../pages/ListPopup.css";
 
-function CreatePostComponent({ updatePosts, show, handleClose }: { updatePosts: () => void, show: boolean, handleClose: () => void}) {
-  const [caption, setCaption] = useState('');
-  const [hashtags, setHashtags] = useState('');
+function CreatePostComponent({
+  updatePosts,
+  show,
+  handleClose,
+}: {
+  updatePosts: () => void;
+  show: boolean;
+  handleClose: () => void;
+}) {
+  const [caption, setCaption] = useState("");
+  const [imageUrl, setImageUrl] = useState(""); // State for the image URL
+  const [hashtags, setHashtags] = useState("");
+  const [uploadedImage, setUploadedImage] = useState<File | null>(null);
   const { username } = useParams();
-  const fileInputRef = useRef<HTMLInputElement>(null); // Ref to handle file input
+  // const fileInputRef = useRef<HTMLInputElement>(null); // Ref to handle file input
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData();
-    if (fileInputRef.current?.files) {
-      formData.append('image', fileInputRef.current.files[0]);
+    if (uploadedImage) {
+      formData.append("image", uploadedImage);
     }
-    formData.append('caption', caption);
-    formData.append('hashtags', hashtags);
+    formData.append("caption", caption);
+    formData.append("hashtags", hashtags);
 
-    console.log(formData)
+    console.log(formData);
 
     try {
-      const response = await axios.post(`${config.serverRootURL}/${username}/createPost`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        withCredentials: true,
-      });
+      const response = await axios.post(
+        `${config.serverRootURL}/${username}/createPost`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true,
+        }
+      );
       console.log(response);
       if (response.status === 201 || response.status === 200) {
-        setCaption('');
-        setHashtags('');
+        setCaption("");
+        setImageUrl(""); // Clear the image URL input
+        setHashtags("");
+        setUploadedImage(null);
         updatePosts();
       }
     } catch (error) {
-      console.error('Error creating post:', error);
+      console.error("Error creating post:", error);
     }
   };
 
   const showHideClassName = show ? "popup display-block" : "popup display-none";
-
 
   return (
     // <div className='w-screen h-screen flex justify-center'>
@@ -82,12 +97,17 @@ function CreatePostComponent({ updatePosts, show, handleClose }: { updatePosts: 
     //   </form>
     // </div>
 
-
     <div className={showHideClassName}>
-        <section className="popup-main">
-        <form onSubmit={handleSubmit}>
-          <div className='flex flex-col space-y-2'>
-            <label htmlFor="caption" className='font-semibold' onClick={handleClose}>Caption</label>
+      <section className="popup-main">
+        <form>
+          <div className="flex flex-col space-y-2">
+            <label
+              htmlFor="caption"
+              className="font-semibold"
+              onClick={handleClose}
+            >
+              Caption
+            </label>
             <textarea
               id="caption"
               placeholder="Caption"
@@ -99,39 +119,96 @@ function CreatePostComponent({ updatePosts, show, handleClose }: { updatePosts: 
             />
           </div>
 
-          <div className='flex flex-col space-y-2'>
-            <label htmlFor="uploadedImage" className='font-semibold'>Upload Image</label>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="uploadedImage" className="font-semibold">
+              Upload Image
+            </label>
             <input
-              ref={fileInputRef}
               id="uploadedImage"
               type="file"
               accept="image/*"
+              onChange={(e) => setCaption(e.target.value)}
               required
             />
           </div>
-    
-          <div className='flex flex-col space-y-2'>
-            <label htmlFor="hashtags" className='font-semibold'>Hashtags</label>
+
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="hashtags" className="font-semibold">
+              Hashtags
+            </label>
             <input
               id="hashtags"
               type="text"
-              className='outline-none bg-white rounded-md border border-gray-300 p-2'
+              className="outline-none bg-white rounded-md border border-gray-300 p-2"
+              value={hashtags}
+              onChange={(e) => setUploadedImage(e.target.files[0])}
+            />
+          </div>
+
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="hashtags" className="font-semibold">
+              Hashtags
+            </label>
+            <input
+              id="hashtags"
+              type="text"
+              className="outline-none bg-white rounded-md border border-gray-300 p-2"
+              value={hashtags}
+              onChange={(e) => setUploadedImage(e.target.files[0])}
+            />
+          </div>
+
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="hashtags" className="font-semibold">
+              Hashtags
+            </label>
+            <input
+              id="hashtags"
+              type="text"
+              className="outline-none bg-white rounded-md border border-gray-300 p-2"
+              value={hashtags}
+              onChange={(e) => setUploadedImage(e.target.files[0])}
+            />
+          </div>
+
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="imageUrl" className="font-semibold">
+              Image URL
+            </label>
+            <input
+              id="imageUrl"
+              type="text"
+              className="outline-none bg-white rounded-md border border-slate-100 p-2"
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="hashtags" className="font-semibold">
+              Hashtags
+            </label>
+            <input
+              id="hashtags"
+              type="text"
+              className="outline-none bg-white rounded-md border border-slate-100 p-2"
               value={hashtags}
               onChange={(e) => setHashtags(e.target.value)}
             />
           </div>
-    
-          <button type="submit" className='w-full mt-4 px-4 py-2 rounded-md bg-indigo-500 text-white font-bold'>Create Post</button>
-          </form>
-        </section>
-      </div>
-    );
-  
+          <div className="flex flex-col space-y-2">
+            <button
+              type="submit"
+              className="px-4 py-2 rounded-md bg-indigo-500 outline-none font-bold text-white"
+              onClick={handleSubmit}
+            >
+              Create Post
+            </button>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 }
-
-
-
-
 
 export default CreatePostComponent;
 
@@ -168,9 +245,6 @@ export default CreatePostComponent;
 //         </div>
 //       </form>
 //     </div>
-
-
-
 
 //   );
 // }
